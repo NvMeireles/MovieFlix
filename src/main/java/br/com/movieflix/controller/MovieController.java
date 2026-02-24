540f6080d3d9d7a6a2ec20b5a24d2aa22c41c444
Nicolas Meireles <nicolasmeireles83@gmail.com>
2026-02-23 22:48:28 -0300

    feat: Criar endpoint para listar filmes

--- src/main/java/br/com/movieflix/controller/MovieController.java
+++ src/main/java/br/com/movieflix/controller/MovieController.java
@@ -1,10 +1,16 @@
 package br.com.movieflix.controller;
 
 
+import br.com.movieflix.controller.request.MovieRequest;
+import br.com.movieflix.controller.response.MovieResponse;
+import br.com.movieflix.entity.Movie;
+import br.com.movieflix.mapper.MovieMapper;
 import br.com.movieflix.service.MovieService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/movieflix/movie")
@@ -13,5 +19,17 @@ public class MovieController {
 
     private final MovieService movieService;
 
+    @PostMapping
+        public ResponseEntity<MovieResponse> save(@RequestBody MovieRequest request){
+        Movie savedMovie = movieService.save(MovieMapper.toMovie(request));
+        return ResponseEntity.ok(MovieMapper.toMovieResponse(savedMovie));
+    }
 
+    @GetMapping
+        public ResponseEntity<List<MovieResponse>> findAll(){
+           return ResponseEntity.ok(movieService.findAll()
+                   .stream()
+                   .map(MovieMapper::toMovieResponse)
+                   .toList());
+        }
 }
--- src/main/java/br/com/movieflix/service/MovieService.java
+++ src/main/java/br/com/movieflix/service/MovieService.java
@@ -1,8 +1,11 @@
 package br.com.movieflix.service;
 
+import br.com.movieflix.entity.Movie;
 import br.com.movieflix.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +13,11 @@ public class MovieService {
 
     private final MovieRepository movieRepository;
 
+    public Movie save(Movie movie){
+        return movieRepository.save(movie);
+    }
+
+    public List<Movie> findAll(){
+        return movieRepository.findAll();
+    }
 }
